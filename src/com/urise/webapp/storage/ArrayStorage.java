3645ff885905a5a00fe1ffee195f329d62906834
CJloHuK <askabelin@gmail.com>
2022-03-11 10:12:37 +0300

    Delete trash comment

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -12,9 +12,6 @@ public class ArrayStorage {
     private int size = 0;
 
     public void clear() {
-//        for (int i = 0; i < size; i++) {
-//            storage[i] = null;
-//        }
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
@@ -26,11 +23,6 @@ public class ArrayStorage {
             return;
         }
         storage[foundedIndex] = r;
-//        for (int i = 0; i < size; i++) {
-//            if (storage[i].getUuid().equals(r.getUuid())) {
-//                storage[i] = r;
-//            }
-//        }
     }
 
     public void save(Resume r) {
@@ -54,29 +46,9 @@ public class ArrayStorage {
             return null;
         }
         return storage[foundedIndex];
-//        for (int i = 0; i < size; i++) {
-//            if (storage[i].getUuid().equals(uuid)) {
-//                return storage[i];
-//            }
-//        }
-//        return null;
     }
 
     public void delete(String uuid) {
-//        int deleteIndex = -1;
-//        for (int i = 0; i < size; i++) {
-//            if (storage[i].getUuid().equals(uuid)) {
-//                deleteIndex = i;
-//                storage[i] = null;
-//                size--;
-//                break;
-//            }
-//        }
-//        if (deleteIndex >= 0) {
-//            if (size - deleteIndex >= 0)
-//                System.arraycopy(storage, deleteIndex + 1, storage, deleteIndex, size - deleteIndex);
-//            storage[size] = null;
-//        }
         int deletedIndex = getIndexOfUuid(uuid);
         if (deletedIndex == -1) {
             System.out.printf("ERROR: %s uuid does not exist\n", uuid);
@@ -85,25 +57,13 @@ public class ArrayStorage {
         storage[deletedIndex] = storage[size - 1];
         storage[size - 1] = null;
         size--;
-//        for (int i = 0; i < size; i++) {
-//            if (storage[i].getUuid().equals(uuid)) {
-//                storage[i] = storage[size - i];
-//                storage[size - i] = null;
-//                size--;
-//                break;
-//            }
-//        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-
         return Arrays.copyOf(storage, size);
-//        Resume[] resumes = new Resume[size];
-//        System.arraycopy(storage, 0, resumes, 0, size);
-//        return resumes;
     }
 
     public int size() {
