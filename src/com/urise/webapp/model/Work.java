b4048347e2c339892d76d37c9fc5c34c1e7c7e77
CJloHuK <askabelin@gmail.com>
2022-04-18 17:12:10 +0300

    Introduce LocalDate to WorkDescription

--- src/com/urise/webapp/model/Work.java
+++ src/com/urise/webapp/model/Work.java
@@ -1,8 +1,14 @@
 package com.urise.webapp.model;
 
+import com.urise.webapp.util.DateUtil;
+
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Work implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -14,8 +20,8 @@ public class Work implements Serializable {
         this.title = title;
     }
 
-    public void addDescription(String dates, String title, String description) {
-        descriptionList.add(new WorkDescription(dates, title, description));
+    public void addDescription(WorkDescription workDescription) {
+        descriptionList.add(workDescription);
     }
 
     @Override
@@ -29,21 +35,42 @@ public class Work implements Serializable {
     }
 
 
-    private class WorkDescription implements Serializable {
+    public class WorkDescription implements Serializable {
         private static final long serialVersionUID = 1L;
-        private String dates;
+        //private String dates;
+        private LocalDate startDate;
+        private LocalDate endDate;
         private String title;
         private String description;
 
-        public WorkDescription(String dates, String title, String description) {
-            this.dates = dates;
+        public WorkDescription(int startYear, Month startMonth, String title, String description) {
+            this(DateUtil.of(startYear, startMonth), DateUtil.NOW, title, description);
+        }
+
+        public WorkDescription(int startYear, Month startMonth, int endYear, Month endMonth, String title, String description) {
+            this(DateUtil.of(startYear, startMonth), DateUtil.of(endYear, endMonth), title, description);
+        }
+
+        public WorkDescription(LocalDate startDate, LocalDate endDate, String title, String description) {
+            Objects.requireNonNull(startDate, "startDate must not be null");
+            Objects.requireNonNull(endDate, "endDate must not be null");
+            Objects.requireNonNull(title, "title must not be null");
+            this.startDate = startDate;
+            this.endDate = endDate;
             this.title = title;
             this.description = description;
         }
 
         @Override
         public String toString() {
-            StringBuilder sb = new StringBuilder(dates);
+            StringBuilder sb = new StringBuilder(startDate.format(DateTimeFormatter.ofPattern("MMM yyyy")));
+            sb.append(" - ");
+            if(endDate.equals(DateUtil.NOW)) {
+                sb.append("настоящее время");
+            }
+            else {
+                sb.append(endDate.format(DateTimeFormatter.ofPattern("MMM yyyy")));
+            }
             sb.append("\t");
             sb.append(title);
             if (description != null) {
