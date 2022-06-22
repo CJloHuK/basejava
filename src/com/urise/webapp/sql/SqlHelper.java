2662e5c9800ae2359bba585a4cb0c84e4bd193db
CJloHuK <askabelin@gmail.com>
2022-06-22 12:44:06 +0300

    HW14_add sections to sql

--- src/com/urise/webapp/sql/SqlHelper.java
+++ src/com/urise/webapp/sql/SqlHelper.java
@@ -1,5 +1,7 @@
 package com.urise.webapp.sql;
 
+import com.urise.webapp.exception.StorageException;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -15,12 +17,28 @@ public class SqlHelper {
         execute(sqlRequest, PreparedStatement::execute);
     }
 
-    public <T> T execute(String sqlRequest, ABlockOfCode<T> aBlockOfCode) {
+    public <T> T execute(String sqlRequest, SqlExecutor<T> sqlExecutor) {
         try (Connection conn = connectionFactory.getConnection();
              PreparedStatement ps = conn.prepareStatement(sqlRequest)) {
-            return aBlockOfCode.execute(ps);
+            return sqlExecutor.execute(ps);
         } catch (SQLException e) {
             throw ExceptionUtil.convertException(e);
+        }
+    }
+
+    public <T> T transactionalExecute(SqlTransaction<T> executor) {
+        try (Connection conn = connectionFactory.getConnection()) {
+            try {
+                conn.setAutoCommit(false);
+                T res = executor.execute(conn);
+                conn.commit();
+                return res;
+            } catch (SQLException e) {
+                conn.rollback();
+                throw ExceptionUtil.convertException(e);
+            }
+        } catch (SQLException e) {
+            throw new StorageException(e);
         }
     }
 }
